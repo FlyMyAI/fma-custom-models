3d84065ca10f5c419a5caf119de0ee124d5cdb6e
n-freman <smiron1703@gmail.com>
2026-02-24 18:59:05 +0500

    add default value support

--- fma/_serialization.py
+++ fma/_serialization.py
@@ -4,6 +4,7 @@ import typing
 from typing import Any, Callable, Dict, Tuple
 
 from pydantic import BaseModel
+from pydantic_core import PydanticUndefined
 
 from fma.toolkit.fields.audio import Audio
 from fma.toolkit.fields.image import Image
@@ -36,18 +37,25 @@ def _parse_io_models(io_class: BaseModel) -> Dict[str, Any]:
     io_fields = []
     for field_name, field_info in io_class.model_fields.items():
         field = {"title": field_name}
-        field["type"], field["optional"] = _get_field_type_data(field_info.annotation)
+        field["type"], field["optional"], default_value = _get_field_type_data(field_info)
+        if default_value is not None:
+            field["default"] = default_value
         io_fields.append(field)
     return {"fields": io_fields}
 
 
-def _get_field_type_data(annotation) -> Tuple[str, bool]:
+def _get_field_type_data(field_info) -> Tuple[str, bool, Any]:
+    annotation = field_info.annotation
     if _is_union(annotation):
         type_ = list(annotation.__args__)[0]
         optional = True
     else:
         type_ = annotation
         optional = False
+
+    default_value = None
+    if (default := field_info.default) != PydanticUndefined:
+        default_value = default
 
     if type_ is str:
         type_string = "string"
@@ -65,7 +73,7 @@ def _get_field_type_data(annotation) -> Tuple[str, bool]:
         type_string = "audio"
     else:
         raise ValueError(f"Type {type_} is not supported")
-    return type_string, optional
+    return type_string, optional, default_value
 
 
 def _is_union(x):
